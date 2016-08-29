a33c6f084060cb01cb467471cf4bde8d22ce0da5
Kevin Chang <changkaimin.big@gmail.com>
2016-08-29 15:00:45 +0800

    modify email provider from nodemailer to sendgrid

--- app/mailers/index.js
+++ app/mailers/index.js
@@ -1,28 +1,25 @@
 'use strict';
 
 var config = require('../config');
-var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: config.gmail.user,
-        pass: config.gmail.pass
-    }
-});
+var sg = require('sendgrid')(config.sgApiKey);
 
 var sendPasswordResetEmail = function (email, html) {
-    var options = {
-        from: '"PicParadise" <info@picparadise.com>',
-        to: email,
-        subject: "Please reset your password on PicParadise",
-        html: html
-    }
-    transporter.sendMail(options, function (err, info) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log(info.response);
-        }
+    var helper = require('sendgrid').mail;
+    var from_email = new helper.Email('info@picparadise.com');
+    var to_email = new helper.Email(email);
+    var subject = 'Please reset your password on PicParadise';
+    var content = new helper.Content('text/html', html);
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+    var request = sg.emptyRequest({
+        method: "POST",
+        path: "/v3/mail/send",
+        body: mail.toJSON(),
+    });
+
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
     });
 };
 
